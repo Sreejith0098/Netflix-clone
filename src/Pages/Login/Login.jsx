1b0f49b3b5f1d3ee11c7f397ce56af9e8b3c0605
Sreejith R <rsreejith090@gmail.com>
2025-10-10 21:38:21 +0530

    netflix loading spinner added in login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,22 +2,28 @@ import React, { useState } from "react";
 import "./Login.css";
 import logo from "../../assets/logo.png";
 import { login, signup } from "../../firebase";
+import netflixSpinner from "../../assets/netflixSpinner.gif"
 
 const Login = () => {
   const [signState, setSignState] = useState("Sign In");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [loading, setLoading] = useState(false)
   const user_auth = async (event) => {
     event.preventDefault();
+    setLoading(true)
     if (signState === "Sign In") {
       await login(email, password);
     } else {
       await signup(name, email, password);
     }
+    setLoading(false)
   };
   return (
+    loading?<div className="login-spinner">
+          <img src={netflixSpinner} alt="" />
+    </div>:
     <div className="login">
       <img src={logo} className="login-logo" alt="" />
       <div className="login-form">
